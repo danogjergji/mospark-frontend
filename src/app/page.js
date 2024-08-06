aa5e5392ed83d1f5a5dfa1ae5dd87acd23404b60
Dano <danogjergji@mosspark.io>
2024-08-05 18:28:19 -0700

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <h1 className="bg-black text-white text-4xl text-center">Project setup</h1>
+    </>
+  )
+}
+
+export default page
